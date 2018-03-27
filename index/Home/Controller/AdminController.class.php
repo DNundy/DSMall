e3589a5ed9baae2918ed6dbff680034e988d1f4c
echo-y <18291953989@163.com>
2018-03-27 21:50:44 +0800

    修改admin

---
+++ index/Home/Controller/AdminController.class.php
@@ -0,0 +1,9 @@
+<?
+// 本类由系统自动生成，仅供测试用途
+namespace Home\Controller;
+use Think\Controller;
+class AdminController extends Controller {
+	public function index(){//显示用户登录页面
+	$this->display('admin');
+    }
+}
--- index/Home/Controller/LoginController.class.php
+++ index/Home/Controller/LoginController.class.php
@@ -18,10 +18,17 @@ class LoginController extends Controller {
     }
 
     public function accept_register(){
-    	$username = $_POST['username'];
+    	$username = $_POST['username'];//获取表单的信息
     	$password = $_POST['password'];
     	$phone = $_POST['phone'];
     	$email = $_POST['email'];
     	$code = $_POST['code'];
+
+    	$verify = new \Think\Verify();   //判断验证的内置方法
+    	if($verify->check($code, $id)){
+    		$user = M('user');//连接数据库
+    		$is_user=$user->where("u_id=$username")->select();//查找该用户是否存在
+    		echo $is_user;
+    	}
     }
 }
