0cf957b430da451acfeb83c44f2566c105cf53c8
echo-y <18291953989@163.com>
2018-03-27 20:35:34 +0800

    fix

--- index/Home/Controller/LoginController.class.php
+++ index/Home/Controller/LoginController.class.php
@@ -4,11 +4,16 @@ namespace Home\Controller;
 use Think\Controller;
 //登录
 class LoginController extends Controller {
-    public function login(){
+    public function login(){//显示用户登录页面
 	$this->display('login');
     }
-    public function register(){
+    public function register(){//显示用户注册页面
 	$this->display('register');
     }
-
+    public function forget(){//显示忘记密码页面
+	$this->display('forget');
+    }
+    public function admin(){//显示管理员登录页面
+	$this->display('admin');
+    }
 }
