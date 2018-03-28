93092d801836d5083b1849aef8abd439775d2107
echo-y <18291953989@163.com>
2018-03-28 10:27:35 +0800

    axios

--- index/Home/Controller/LoginController.class.php
+++ index/Home/Controller/LoginController.class.php
@@ -16,17 +16,21 @@ class LoginController extends Controller {
     public function login_admin(){//显示管理员登录页面
 	$this->display('login-admin');
     }
-
+    //接收并处理注册页面传来的数据
     public function accept_register(){
     	//获取表单中的信息
+    	$code = $_POST['code'];
     	$data = array(
-    		'u_id' => $_POST['num'], 
-    		'u_name' => $_POST['name'],
-    		'u_password' => $_POST['password'],
-    		'u_email' => $_POST['email'],
-    		'u_place' => $_POST['place'],
-    		'u_telphone' => $_POST['phone'],
+    		'u_id' => $_POST['num'], //账户
+    		'u_name' => $_POST['name'],//姓名
+    		'u_password' => md5($_POST['password']),//用MD5对密码进行加密
+    		'u_email' => $_POST['email'],//邮箱
+    		'u_place' => $_POST['place'],//交易的地址
+    		'u_telphone' => $_POST['phone'],//联系电话
     	);
+    	var_dump($_REQUEST);
+    	var_dump($code);
+    	var_dump($data);
     	$verify = new \Think\Verify();   //判断验证的内置方法
     	if($verify->check($code, $id)){
     		$user = M('user');//连接数据库
@@ -37,6 +41,23 @@ class LoginController extends Controller {
     		else{
     			$this->error('该用户已存在!');
     		}
+    	}
+    	else{
+    		$this->error('验证码错误!');
+    	}
+    }
+    public function accept_login(){
+    	//获取登录页面表单中的信息
+    	$code = $_POST['code'];
+    	$data = array(
+    		'u_id' => $_POST['num'], //账户
+    		'u_password' => md5($_POST['password']),//用MD5对密码进行加密
+    	);
+    	if($verify->check($code, $id)){
+
+    	}
+    	else{
+    		$this->error('验证码错误!');
     	}
     }
 }
