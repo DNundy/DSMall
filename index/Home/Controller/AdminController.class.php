aabdbcc238d9ce17c956d5a58c0809c7be878991
echo-y <18291953989@163.com>
2018-03-29 17:34:37 +0800

    json

--- index/Home/Controller/AdminController.class.php
+++ index/Home/Controller/AdminController.class.php
@@ -9,7 +9,8 @@ class AdminController extends Controller {
     public function findNotice(){//显示系统发布的所有通知
     	$notice = M('Notice');
     	$adminNotice = $notice->where()->select();
-    	return $this->ajaxReturn($adminNotice,'JSON');
+    	$res['msg'] = $adminNotice;
+    	return $this->ajaxReturn($res);
     }
     public function addNotice(){
 
