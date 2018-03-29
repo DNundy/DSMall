232621ef8555b4408c43e139c31eb45519623a4f
echo-y <18291953989@163.com>
2018-03-29 10:33:39 +0800

    发送邮件完成

--- index/Common/Conf/config.php
+++ index/Common/Conf/config.php
@@ -14,7 +14,7 @@ return array(
     'MAIL_SMTPAUTH' =>TRUE, //启用smtp认证
     'MAIL_USERNAME' =>'18291953989@163.com',//你的邮箱名
     'MAIL_FROM' =>'18291953989@163.com',//发件人地址
-    'MAIL_FROMNAME'=>'胖胖二手交易平台',//发件人姓名
+    'MAIL_FROMNAME'=>'趣二手交易平台',//发件人姓名
     'MAIL_PASSWORD' =>'199723s',//邮箱密码
     'MAIL_CHARSET' =>'utf-8',//设置邮件编码
     'MAIL_ISHTML' =>TRUE, // 是否HTML格式邮件
