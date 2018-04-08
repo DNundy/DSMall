918c3a7211d441714c87f332a20c17722c62fd16
echo-y <18291953989@163.com>
2018-04-08 19:43:58 +0800

    fix

--- index/Home/Controller/AdminUserGoodsController.class.php
+++ index/Home/Controller/AdminUserGoodsController.class.php
@@ -68,7 +68,6 @@ class AdminUserGoodsController extends AdminCommonController {
 		$Model = new Model();
  		$sql ="select * from trading_goods left join trading_user on trading_goods.u_id=trading_user.u_id order by g_id desc";
  		$goodsInfo = $Model->query($sql);
- 		echo json_encode($goodsInfo);exit;
 		if(!empty($goodsInfo)){
     		$res = array(
     			'code' => '0',
