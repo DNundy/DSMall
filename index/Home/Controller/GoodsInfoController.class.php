4b6de4e8b5429c31bf89a771c81c8e6d544f0985
echo-y <18291953989@163.com>
2018-04-24 15:39:00 +0800

    展示商品发布时间+格式化

--- index/Home/Controller/GoodsInfoController.class.php
+++ index/Home/Controller/GoodsInfoController.class.php
@@ -2,7 +2,8 @@
 // 本类由系统自动生成，仅供测试用途
 namespace Home\Controller;
 use Think\Controller;
-//登录
+use Think\Model;
+
 class GoodsInfoController extends Controller {
 	public function index(){
 		if(empty($_GET['id'])){
@@ -13,17 +14,29 @@ class GoodsInfoController extends Controller {
             return $this->ajaxReturn($res);
 		}
 		$id = $_GET['id'];
-		$info = M('Goods')->where("g_id = $id")->select();
-		if(empty($info)){
+		$Model = new Model();
+ 		$sql ="select trading_goods.g_name,
+ 					  trading_goods.g_construct,
+ 					  trading_goods.g_type,
+ 					  trading_goods.g_price,
+ 					  trading_goods.g_time,
+ 					  trading_goods.g_picture,
+ 					  trading_user.u_name,
+ 					  trading_user.u_place,
+ 					  trading_user.u_telphone
+ 					  from trading_goods left join trading_user on trading_goods.u_id=trading_user.u_id where trading_goods.g_id=$id";
+ 		$goodsInfo = $Model->query($sql);
+		if(empty($goodsInfo)){
 			$res = array(
                 'code' => '-1',
                 'msg' => "抱歉该商品已下架！",
         	);
         	return $this->ajaxReturn($res);
 		}
+		$goodsInfo[0]['g_time'] = date("Y-m-d H:i:s",$goodsInfo[0]['g_time']);
 		$res = array(
                 'code' => '0',
-                'msg' => $info,
+                'msg' => $goodsInfo,
         );
         return $this->ajaxReturn($res);
 	}
